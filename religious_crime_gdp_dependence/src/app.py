956ea770695e74251cfd02f3014b3c6bfde7ecc0
lymuthien <tanya120606@mail.ru>
2025-06-19 21:52:43 +0300

    Final calcs

--- religious_crime_gdp_dependence/src/app.py
+++ religious_crime_gdp_dependence/src/app.py
@@ -13,7 +13,7 @@ class App(object):
         self._statistics_calculator = StatisticsCalculator()
         self._df_factory = DfDependenceFactory()
         self._plotter = DfDependencePlotter()
-        self._dependence_dfs: dict[str, pd.DataFrame] = {}
+        self._new_dfs: dict[str, pd.DataFrame] = {}
         self._min_year = 2015
         self._max_year = 2023
 
@@ -22,7 +22,6 @@ class App(object):
         crime_gdp_df = pd.merge(
             gdp_df, crime_df, on=["Country Name", "Year"], how="inner"
         )
-        return crime_gdp_df
         crime_religious_gdp_df = pd.merge(
             religious_df, crime_gdp_df, on=["Country Name", "Year"], how="inner"
         )
@@ -36,38 +35,49 @@ class App(object):
         return crime_df, religious_df, gdp_df
 
     def _plot_correlation(self):
-        for k, df in self._dependence_dfs.items():
+        for k, df in self._new_dfs.items():
             self._plotter.plot_correlation_over_years(
                 df, f"GDP-{k} Correlation", "YEAR", "CORRELATION"
             )
 
     def _create_dependence_df(self):
-        self._dependence_dfs["crime_by_gdp"] = self._df_factory.create_df()
-        # self._dependence_dfs["religious_by_crime"] = self._df_factory.create_df()
+        self._new_dfs["crime_by_gdp"] = self._df_factory.create_df()
+        self._new_dfs["muslim_crime_gdp"] = self._df_factory.create_df()
+        self._new_dfs["christian_crime_gdp"] = self._df_factory.create_df()
+        # self._new_dfs["catholic_crime_gdp"] = self._df_factory.create_df()
 
     def _save_dependence_df(self):
-        for k, df in self._dependence_dfs.items():
+        for k, df in self._new_dfs.items():
             df.to_csv(self._dir / f"{k}_dependence.csv")
 
     def _make_tables(self):
         merged_df = self._get_merged_df()
-        comparer_columns = ("GDP",)
+        dfs = (
+            merged_df,
+            merged_df[merged_df["Religion"] == "Muslim"],
+            # merged_df[merged_df["Religion"] == "Christian"],
+            merged_df[
+                (merged_df["Religion"] == "Catholic")
+                | (merged_df["Religion"] == "Christian")
+                | (merged_df["Religion"] == "Orthodox")
+                | (merged_df["Religion"] == "Protestant")
+                | (merged_df["Religion"] == "Lutheran")
+            ],
+        )
+        years = range(self._min_year, self._max_year + 1)
+        comparer_column = "GDP"
         comparable_column = "Crime Index"
 
-        for year in range(self._min_year, self._max_year + 1):
-            current_df = merged_df[merged_df["Year"] == year]
+        for df, new_df in zip(dfs, self._new_dfs.values()):
+            for year in years:
+                current_df = df[df["Year"] == year]
 
-            for df, comparer_column in zip(
-                self._dependence_dfs.values(),
-                comparer_columns,
-            ):
                 stats = self._statistics_calculator.calculate_dependence_statistics(
                     current_df, comparer_column, comparable_column, str(year)
                 )
-                df.loc[len(df)] = stats
+                new_df.loc[len(new_df)] = stats
 
-            if year == self._max_year or year == self._min_year or year % 5 == 0:
-                for comparer_column in comparer_columns:
+                if year == self._max_year or year == self._min_year or year % 5 == 0:
                     self._plotter.plot_dependence(
                         df=current_df,
                         comparer_column=comparer_column,
